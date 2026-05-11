c1369ac5235cd297f4e280f240b94b880f784e31
HanlanY <yanghanlan666@gmail.com>
2026-05-11 22:49:18 +0000

    feat(vis): add a small script to test visualizer in local folder.

--- ogbench/__init__.py
+++ ogbench/__init__.py
@@ -1,9 +1,19 @@
 """OGBench: Benchmarking Offline Goal-Conditioned RL"""
 
-import ogbench.locomaze
-import ogbench.manipspace
-import ogbench.powderworld
+import sys
+import types
+from pathlib import Path
+
+import ogbench.locomaze as locomaze
+import ogbench.manipspace as manipspace
+import ogbench.powderworld as powderworld
 from ogbench.utils import download_datasets, load_dataset, make_env_and_datasets
+
+_LOCAL_DIR = Path(__file__).resolve().parents[1] / 'local'
+if _LOCAL_DIR.exists() and 'ogbench.local' not in sys.modules:
+    local_module = types.ModuleType('ogbench.local')
+    local_module.__path__ = [str(_LOCAL_DIR)]
+    sys.modules['ogbench.local'] = local_module
 
 __all__ = (
     'locomaze',
